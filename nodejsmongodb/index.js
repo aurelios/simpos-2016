45113dbd782c66eafbacb83561fc1279c3fff1fc
aurelios <dicios@gmail.com>
2016-09-12 09:40:02 -0300

    first commit

---
+++ nodejsmongodb/index.js
@@ -0,0 +1,77 @@
+const path = require('path')  
+const exphbs = require('express-handlebars')
+const express = require('express')
+const app = express()
+var assert = require('assert');
+const port = 3000
+
+var MongoClient = require('mongodb').MongoClient;
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John'
+  })
+})
+	
+var insertDocument = function(db, callback) {
+   db.collection('users').insertMany([ 
+	   {
+         "name" : "aurelio",
+         "age" : 26         
+      },{
+         "name" : "maria",
+         "age" : 15         
+      }
+	], function(err, result) {
+    assert.equal(err, null);
+    console.log("Inserted a document into the restaurants collection.");
+    callback();
+  });
+};
+
+app.get('/usersi', (request, response) => {
+	var users = {Users:[]};
+	var url = 'mongodb://localhost:27017/test';
+	MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+	  console.log("Connected correctly to server.");
+		insertDocument(db, function() {
+			  db.close();
+		});
+	})	  
+});
+
+app.get('/users', (request, response) => {
+	var users = {Users:[]};
+	var url = 'mongodb://localhost:27017/test';
+	MongoClient.connect(url, function(err, db) {
+		assert.equal(null, err);
+		console.log("Connected correctly to server.");
+	  	  
+		db.collection('users').find().toArray(function(err, drinks) {
+			users.Users = drinks;
+			console.log(drinks);
+			db.close();
+			response.render('users', users);
+		});
+	  
+	});	
+});
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+  console.log('server is listening on '+port)
+})
---
+++ nodejspg/index.js
@@ -0,0 +1,66 @@
+const path = require('path')  
+const exphbs = require('express-handlebars')
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use('/assets', express.static(__dirname + '/assets'));
+
+app.engine('.hbs', exphbs({  
+  defaultLayout: 'main',
+  extname: '.hbs',
+  layoutsDir: path.join(__dirname, 'views/layouts')
+}))
+
+app.set('view engine', '.hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', (request, response) => {
+  response.render('home', {
+    name: 'John'
+  })
+})
+
+app.get('/users', (request, response) => {
+	const pg = require('pg')  
+	const conString = 'postgres://postgres:Rb5!!!!!@localhost/postgres' // make sure to match your own database's credentials
+
+	  pg.connect(conString, function (err, client, done) {
+		if (err) {
+		  // pass the error to the express error handler
+		  return next(err)
+		}	
+		
+		var users = {Users:[]};
+		client.query('SELECT name, age FROM users;', [], function (err, result) {
+			done()
+			if (err) {
+			// pass the error to the express error handler
+			return next(err)
+			}
+			users.Users  = result.rows;
+			response.render('users',users);
+		})
+		
+		/*var query = client.query('SELECT name, age FROM users;')
+		
+		query.on('row', function(row, response) {
+			console.log(row);
+			users.Users.push(row);
+		})
+		
+		query.on("end", function (result) {
+			client.end();
+			console.log(users);			
+			response.render('users',users);
+		});	*/	
+	  }) 
+})
+
+app.listen(port, (err) => {  
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log('server is listening on ${port}')
+})
